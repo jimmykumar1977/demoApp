f314b6fe164e8b69cfb62eece7dbc0eadeb6a97f
unknown <jimmy.kumar@gmail.com>
2016-11-10 23:07:04 +0530

    updated project structure and build.gradle

---
+++ src/main/java/demoApp/StatusService.java
@@ -0,0 +1,14 @@
+package demoApp;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/status")
+public class StatusService {
+
+	  @GET
+	  public String status() {
+	    String output = "UP";
+	    return output;
+	  }
+}
